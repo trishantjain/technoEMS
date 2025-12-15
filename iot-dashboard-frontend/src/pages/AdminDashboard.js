e57d8bb5bd66629c373c2191e994df8252c662bb
trishantjain4444 <trishantjain4444@gmail.com>
2025-12-15 18:00:47 +0530

    Updated AdminDash for IP

--- iot-dashboard-frontend/src/pages/AdminDashboard.js
+++ iot-dashboard-frontend/src/pages/AdminDashboard.js
@@ -630,11 +630,11 @@ const EditableRow = ({ device, onUpdated }) => {
           <input
             type="text"
             name="ipCamera"
-            value={formData.ipCamera}
+            value={formData.ipCamera.ip}
             onChange={handleChange}
           />
-        ) : device.ipCamera ? (
-          <a href={device.ipCamera} target="_blank" rel="noopener noreferrer">
+        ) : device.ipCamera.ip ? (
+          <a href={`https://${device.ipCamera.ip}`} target="_blank" rel="noopener noreferrer">
             📷 View
           </a>
         ) : (
